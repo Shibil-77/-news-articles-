d2382827b7c707c50bd4e51ad0179ee9333ee3c2
Shibil-77 <shibi2772@gmail.com>
2023-04-01 11:29:31 +0530

    banner completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import HomePage from "./Pages/HomePage/HomePage";
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
- <>
-   <HomePage/>
- </>
+    <>
+      <Router>
+        <HomePage />
+      </Router>
+    </>
   );
 }
 
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Banner.css'
+import axios from "axios"
+
 
 function Banner() {
-     
-    
-     
+  const [news, setNews] = useState()
+
+  const bannerDataFetch = async () => {
+    const { data } = await axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=05af86a9d5f7473ca489d930efaf74dc")
+    setNews(data.articles[Math.round(Math.random() * data.articles.length)])
+  }
+
+  useEffect(() => {
+    bannerDataFetch()
+  }, [])
+
   return (
     <>
-     <div
-     style={{backgroundImage:`url(${"https://www.pngitem.com/pimgs/m/527-5272454_white-bbc-news-logo-png-transparent-png.png"})`}} className='Banner'>
-      <div className='content'>
-        <h1 className='title'>hello worl</h1>
-        <div className='Banner_btn'>
-            <button type="" className='btn'>Play</button>
-            <button type="" className='btn'>My List</button>
-        </div>
-        <h1 className='discription'>hello</h1>
-      </div>
-      <div className='fade-bottom'></div>
-    </div>
+      {news &&
+        <div
+          style={{ backgroundImage: `url(${news.urlToImage})` }} className='Banner'>
+          <div className='content'>
+            <h1 className='title'>{news.title}</h1>
+            <div className='Banner_btn'>
+              <Link to={news.url}>
+                <button type="" className='btn'>View</button>
+              </Link>
+            </div>
+            <h1 className='description'>{news.content}</h1>
+          </div>
+        </div>}
     </>
   )
 }
